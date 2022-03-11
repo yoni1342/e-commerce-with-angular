f99fc632685e5d971d9d064d1d8c40c5982df7fb
yoni <yoni@kali>
2022-03-11 03:38:47 +0300

    PUSH

--- src/app/customer/customer.object.ts
+++ src/app/customer/customer.object.ts
@@ -17,6 +17,7 @@ export class Customer{
 
 export const CUSTOMERS: Customer[] = [
     {id:0, name: 'Helen', age: 23, imageUrl:'../../assets/cast1.png', location: 'Ethiopia', duration: 3},
+    {id:0, name: 'yami', age: 21, imageUrl:'../../assets/yami.JPG', location: 'Ethiopia', duration: 3},
     {id:1, name: 'Bill Gates', age: 57, imageUrl:'../../assets/bil.jpg', location: 'USA', duration: 2},
     {id:2, name: 'Josi', age: 19, imageUrl:'../../assets/cust2.jpg', location: 'Ethiopia', duration: 1},
     {id:2, name: 'NaNi', age: 24, imageUrl:'https://i.pinimg.com/originals/f7/1e/3e/f71e3e88ac85fc3fed5d7d8a1b0d45ad.jpg', location: 'Ethiopia', duration:0},
